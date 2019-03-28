2cd8937d275889b37dac95f09b2e95d44a8cb7e6
Sujin Lee <krispediadot@gmail.com>
2019-03-28 23:11:47 +0900

    update func

--- assign03.cpp
+++ assign03.cpp
@@ -6,20 +6,19 @@
 
 using namespace std;
 
-int N[3] = {500, 10000, 100000};
+int N[3] = {1000, 10000, 100000};
 double runningTime[9][6] = {0.0};
 vector<double> randomTime(10,0);
-vector<double> randomTime_origin(10,0);
+vector<double> randomTimeOrigin(10,0);
 vector<int> data;
-vector<int> data_origin;
+vector<int> dataOrigin;
 
 void merge(vector<int> data, int p, int q, int r);
-int partition_last_pivot(int left, int right);
-int partition_median_pivot(int left, int right);
-int partition_first_pivot(int left, int right);
-int partition_random_pivot(int left, int right);
+int partitionLastPivot(int left, int right);
+int partitionMedianPivot(int left, int right);
+int partitionRandomPivot(int left, int right);
 
-void print_result() {
+void printResult() {
 
     cout<<"            \tRandom1000";
 
@@ -49,29 +48,10 @@ void print_result() {
             cout<<"\tQuick2"; 
         else if(i==6)
             cout<<"\tQuick3";                    
-        //cout<<"Bubble Sort";
-        /*
-        switch (i){
-            cout.flags(ios::right);
-            case 0: cout<<"Bubble sort";
-            cout.flags(ios::right);
-            case 1: cout<<"Selection sort";
-            cout.flags(ios::right);
-            case 2: cout<<"Insertion sort";
-            cout.flags(ios::right);
-            case 3: cout<<"Merge";
-            cout.flags(ios::right);
-            case 4: cout<<"Quick1";
-            cout.flags(ios::right);
-            case 5: cout<<"Quick2";
-            cout.flags(ios::right);
-            case 6: cout<<"Quick3";
-        }
-        */
         
         for(int j=0; j<6; j++){
             cout.width(12);
-            cout<<runningTime[i][j]<<"sec ";
+            cout<<runningTime[i][j];
         }
         cout<<'\n';
     }
@@ -81,7 +61,7 @@ void swap(int index1, int index2){
     data.at(index1) = data.at(index2);
     data.at(index2) = temp;
 }
-void bubble_sort(){
+void bubbleSort(){
     for(int  i = data.size()-1; i>=0; i--){
         for(int j = 1; j<=i; j++){
             if(data.at(j-1)>data.at(j))
@@ -89,7 +69,7 @@ void bubble_sort(){
         }
     }
 }
-void selection_sort(){
+void selectionSort(){
     for(int i=data.size()-1; i>=0; i--){
         int max = data.at(i);
         int maxIndex = i;
@@ -103,7 +83,7 @@ void selection_sort(){
             swap(i,maxIndex);
     }
 }
-void insertion_sort(){
+void insertionSort(){
     for(int i=1; i<data.size(); i++){
         int temp = data.at(i);
         int locIndex = 0;
@@ -126,12 +106,12 @@ void insertion_sort(){
         data.at(locIndex) = temp;
     }
 }
-void merge_sort(vector<int> arr, int left, int right){
+void mergeSort(vector<int> arr, int left, int right){
 //    cout<<"merge sort start!"<<'\n';
     if(left<right){
         int mid = (left+right)/2;
-        merge_sort(arr, left, mid);
-        merge_sort(arr, mid+1, right);
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid+1, right);
         merge(data, left, mid, right);
     }
 }
@@ -169,14 +149,14 @@ void merge(vector<int> arr, int left, int mid, int right){
         cout<<'\n';
 */
 }
-void quick_sort_last(int left, int right){
+void quickSortLast(int left, int right){
     if(left<right){
-        int pivot = partition_last_pivot(left, right);
-        quick_sort_last(left, pivot-1);
-        quick_sort_last(pivot+1, right);
+        int pivot = partitionLastPivot(left, right);
+        quickSortLast(left, pivot-1);
+        quickSortLast(pivot+1, right);
     }
 }
-int partition_last_pivot(int left, int right){
+int partitionLastPivot(int left, int right){
 //    cout<<"partition start!"<<'\n';
     int i=left-1;
     //int j=left;
@@ -193,15 +173,15 @@ int partition_last_pivot(int left, int right){
 
     return i+1;
 }
-void quick_sort_median(int left, int right){
+void quickSortMedian(int left, int right){
     if(left<right){
-        int pivot = partition_median_pivot(left, right);
-        quick_sort_last(left, pivot-1);
-        quick_sort_last(pivot+1, right);
+        int pivot = partitionMedianPivot(left, right);
+        quickSortLast(left, pivot-1);
+        quickSortLast(pivot+1, right);
     }
     
 }
-int get_median(){
+int getMedian(){
     int mediumIndex = data.size()/2;
     int lastIndex = data.size()-1;
 //    cout<<"mediumIndex: "<<mediumIndex<<'\n';
@@ -212,12 +192,12 @@ int get_median(){
     else
         return lastIndex;
 }
-int partition_median_pivot(int left, int right){
+int partitionMedianPivot(int left, int right){
 //    cout<<"partition start!"<<'\n';
     int i=left-1;
     //int j=left;
 
-    int checkIndex = get_median();
+    int checkIndex = getMedian();
     swap(checkIndex, right);
 
     int check = data.at(right);
@@ -232,15 +212,15 @@ int partition_median_pivot(int left, int right){
 
     return i+1;
 }
-void quick_sort_random(int left, int right){
+void quickSortRandom(int left, int right){
     if(left<right){
-        int pivot = partition_random_pivot(left, right);
-        quick_sort_last(left, pivot-1);
-        quick_sort_last(pivot+1, right);
+        int pivot = partitionRandomPivot(left, right);
+        quickSortLast(left, pivot-1);
+        quickSortLast(pivot+1, right);
     }
     
 }
-int partition_random_pivot(int left, int right){
+int partitionRandomPivot(int left, int right){
 //    cout<<"partition start!"<<'\n';
     int i=left-1;
     //int j=left;
@@ -264,17 +244,17 @@ int partition_random_pivot(int left, int right){
 
 void generateRandomData(int N){
     srand((unsigned int) time(NULL));
-    data_origin.clear();
+    dataOrigin.clear();
     for(int i=0; i<N; i++){
         // random 수 생성
         int number = rand()%N+1;
-        data_origin.push_back(number);
+        dataOrigin.push_back(number);
     }
 }
 void generateReversedData(int N){
-    data_origin.clear();
+    dataOrigin.clear();
     for(int i=0; i<N; i++){
-        data_origin.push_back(N-i);
+        dataOrigin.push_back(N-i);
     }
 }
 void printData(){
@@ -282,50 +262,37 @@ void printData(){
         cout<<*it<<" ";
     cout<<'\n';    
 }
-void addRunningTime(double time){
-
-}
 void saveRunningTime(int row, int col, double time){
     runningTime[row][col] = time;
 }
 void runSort(int i, int j) {
 
-    cout<<"randomTime_origin"<<'\n';
-    for(auto it=randomTime_origin.begin(); it!=randomTime_origin.end(); ++it)
-        cout<<*it<<" ";
-    cout<<'\n';
-    cout<<"randomTime"<<'\n';
-    for(auto it=randomTime.begin(); it!=randomTime.end(); ++it)
-        cout<<*it<<" ";
-    cout<<'\n';
-
-
     clock_t start, end; 
     
-    data.resize(data_origin.size());
+    data.resize(dataOrigin.size());
                         
-    copy(data_origin.begin(), data_origin.end(), data.begin());
-    printData();
+    copy(dataOrigin.begin(), dataOrigin.end(), data.begin());
+    //printData();
     start = clock();
     cout<<"bubble sort"<<'\n';
-    bubble_sort();
+    bubbleSort();
     end = clock();
     double resultTime = (double)(end-start)/CLOCKS_PER_SEC;
     if(j == 0){
         double temp = resultTime;
-        cout<<"resultTIME: "<<resultTime<<'\n';
+        //cout<<"resultTIME: "<<resultTime<<'\n';
         resultTime += randomTime.at(0);
         randomTime.at(0) += temp;
     }
     saveRunningTime(0,(i+1)*(j+1)-1, resultTime);
     //runningTime[0][(i+1)*(j+1)-1] = (double)(end-start)/CLOCKS_PER_SEC;
-    printData();
+    //printData();
 
-    copy(data_origin.begin(), data_origin.end(), data.begin());
-    printData();
+    copy(dataOrigin.begin(), dataOrigin.end(), data.begin());
+    //printData();
     start = clock();
     cout<<"selection sort"<<'\n';
-    selection_sort();
+    selectionSort();
     end = clock();
     resultTime = (double)(end-start)/CLOCKS_PER_SEC;
     if(j == 0){
@@ -335,13 +302,13 @@ void runSort(int i, int j) {
     }
     saveRunningTime(1,(i+1)*(j+1)-1,resultTime);
     //runningTime[1][(i+1)*(j+1)-1] = (double)(end-start)/CLOCKS_PER_SEC;
-    printData();
+    //printData();
 
-    copy(data_origin.begin(), data_origin.end(), data.begin());
-    printData();           
+    copy(dataOrigin.begin(), dataOrigin.end(), data.begin());
+    //printData();           
     start = clock();
     cout<<"insertion sort"<<'\n';
-    insertion_sort();
+    insertionSort();
     end = clock();
     resultTime = (double)(end-start)/CLOCKS_PER_SEC;
     if(j == 0){
@@ -351,13 +318,13 @@ void runSort(int i, int j) {
     }
     saveRunningTime(2,(i+1)*(j+1)-1,resultTime);
     //runningTime[2][(i+1)*(j+1)-1] = (double)(end-start)/CLOCKS_PER_SEC;
-    printData();
+    //printData();
 
-    copy(data_origin.begin(), data_origin.end(), data.begin());
-    printData();          
+    copy(dataOrigin.begin(), dataOrigin.end(), data.begin());
+    //printData();          
     start = clock();
     cout<<"merge sort"<<'\n';
-    merge_sort(data, 0, N[i]-1);
+    mergeSort(data, 0, N[i]-1);
     end = clock();
     resultTime = (double)(end-start)/CLOCKS_PER_SEC;
     if(j == 0){
@@ -367,13 +334,13 @@ void runSort(int i, int j) {
     }
     saveRunningTime(3,(i+1)*(j+1)-1,resultTime);
     //runningTime[3][(i+1)*(j+1)-1] = (double)(end-start)/CLOCKS_PER_SEC;
-    printData();
+    //printData();
     // pivot 
-    copy(data_origin.begin(), data_origin.end(), data.begin());
-    printData();          
+    copy(dataOrigin.begin(), dataOrigin.end(), data.begin());
+    //printData();          
     start = clock();
     cout<<"quick_sort_last"<<'\n';
-    quick_sort_last(0, N[i]-1);
+    quickSortLast(0, N[i]-1);
     end = clock();
     resultTime = (double)(end-start)/CLOCKS_PER_SEC;
     if(j == 0){
@@ -383,13 +350,13 @@ void runSort(int i, int j) {
     }
     saveRunningTime(4,(i+1)*(j+1)-1,resultTime);
     //runningTime[4][(i+1)*(j+1)-1] = (double)(end-start)/CLOCKS_PER_SEC;
-    printData();
+    //printData();
             
-    copy(data_origin.begin(), data_origin.end(), data.begin());
-    printData();
+    copy(dataOrigin.begin(), dataOrigin.end(), data.begin());
+    //printData();
     start = clock();
     cout<<"quick_sort_median"<<'\n';
-    quick_sort_median(0, N[i]-1);
+    quickSortMedian(0, N[i]-1);
     end = clock();
     resultTime = (double)(end-start)/CLOCKS_PER_SEC;
     if(j == 0){
@@ -399,13 +366,13 @@ void runSort(int i, int j) {
     }
     saveRunningTime(5,(i+1)*(j+1)-1,resultTime);
     //runningTime[5][(i+1)*(j+1)-1] = (double)(end-start)/CLOCKS_PER_SEC;
-    printData();
+    //printData();
             
-    copy(data_origin.begin(), data_origin.end(), data.begin());
-    printData();
+    copy(dataOrigin.begin(), dataOrigin.end(), data.begin());
+    //printData();
     start = clock();
     cout<<"quick_sort_random"<<'\n';
-    quick_sort_random(0, N[i]-1);
+    quickSortRandom(0, N[i]-1);
     end = clock();
     resultTime = (double)(end-start)/CLOCKS_PER_SEC;
     if(j == 0){
@@ -415,31 +382,20 @@ void runSort(int i, int j) {
     }
     saveRunningTime(6,(i+1)*(j+1)-1,resultTime);
     //runningTime[6][(i+1)*(j+1)-1] = (double)(end-start)/CLOCKS_PER_SEC;
-    printData();   
+    //printData();   
 }
 void runningTest(){
-    for(int i=0; i<T-2; i++) {
+    for(int i=0; i<T; i++) {
        for(int j=0; j<2; j++){
            clock_t start, end; 
            if(j==0){
-                copy(randomTime_origin.begin(), randomTime_origin.end(), randomTime.begin());
-                cout<<"randomTime_origin"<<'\n';
-                for(auto it=randomTime_origin.begin(); it!=randomTime_origin.end(); ++it)
-                    cout<<*it<<" ";
-                cout<<'\n';
-                cout<<"randomTime"<<'\n';
-                for(auto it=randomTime.begin(); it!=randomTime.end(); ++it)
-                    cout<<*it<<" ";
-                cout<<'\n';                
+                copy(randomTimeOrigin.begin(), randomTimeOrigin.end(), randomTime.begin());
+     ;                
                 for(int k=0; k<3; k++){
                     generateRandomData(N[i]);
                     //double result = (double)(end-start)/CLOCKS_PER_SEC;
                     runSort(i, j);
 
-                }
-                cout<<"runningTime"<<'\n';
-                for(int k=0; k<7; k++){
-                    cout<<runningTime[k][(i+1)*(j+1)-1]<<'\n';
                 }
                 for(int k=0; k<7; k++){
                     runningTime[k][(i+1)*(j+1)-1] /= 3.0;
@@ -456,38 +412,9 @@ void runningTest(){
 
 int main(void) {
     ios_base::sync_with_stdio(false);
-/*
-    clock_t start, end;
 
-    start = clock();
-
-//    generateReversedData();
-    generateRandomData(10);  
-    for(auto it=data_origin.begin(); it!=data_origin.end(); ++it)
-        cout<<*it<<" ";
-    cout<<'\n';
-    
-    data.resize(data_origin.size());
-    copy(data_origin.begin(), data_origin.end(), data.begin());
-//    merge_sort(data, 0, data.size()-1);
-    quick_sort_last(0, data.size()-1);
-
-//    bubble_sort();
-    for(auto it=data.begin(); it!=data.end(); ++it)
-        cout<<*it<<" ";
-    cout<<'\n';
-
-    for(auto it=data_origin.begin(); it!=data_origin.end(); ++it)
-        cout<<*it<<" ";
-    cout<<'\n';
-
-
-    end = clock();
-    double result = (double)(end-start)/CLOCKS_PER_SEC;
-    cout<<"걸린 시간: "<<result<<"sec"<<'\n';
-*/
     runningTest();
-    print_result();
+    printResult();
 
     return 0;
 }
